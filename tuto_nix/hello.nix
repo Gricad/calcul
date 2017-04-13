f3d219803f7a3ca651412fdb0282cedfb44721cf
Bruno Bzeznik <Bruno.Bzeznik@imag.fr>
2017-04-13 10:57:21 +0200

    Added simple standalone package example

---
+++ tuto_nix/hello.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+
+let
+  inherit stdenv fetchurl perl;
+  appName = "hello-2.1.1y";
+in
+{
+  "${appName}" = stdenv.mkDerivation {
+    name = appName;
+    builder = ./builder.sh;
+    src = fetchurl {
+      url = ftp://ftp.nluug.nl/pub/gnu/hello/hello-2.1.1.tar.gz;
+      md5 = "70c9ccf9fac07f762c24f2df2290784d";
+    };
+    inherit perl;
+  };
+}
